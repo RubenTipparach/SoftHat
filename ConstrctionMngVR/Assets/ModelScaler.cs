58645eacbe462a1dacbd566117e41214a47a842d
Rubensaccnt <santipab.stephane@gmail.com>
2016-10-14 15:12:19 -0500

    Initial Checkin

---
+++ ConstrctionMngVR/Assets/ModelScaler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelScaler : MonoBehaviour {
+
+    float scaleTransform = 1;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Equals))
+        {
+            scaleTransform += .1f;
+            transform.localScale = (new Vector3(scaleTransform, scaleTransform, scaleTransform));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            scaleTransform -= .1f;
+            transform.localScale = (new Vector3(scaleTransform, scaleTransform, scaleTransform));
+        }
+
+    }
+}
---
+++ ConstrctionMngVR/Assets/SpawnNotes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNotes : MonoBehaviour {
+
+    [SerializeField]
+    GameObject spawnanbleNote;
+
+    [SerializeField]
+    Camera camera;
+
+    [SerializeField]
+    float height;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Plane groundPlane = new Plane(Vector3.up, 0);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            float rayDistance;
+
+            if (groundPlane.Raycast(ray, out rayDistance))
+            {
+                GameObject.Instantiate(spawnanbleNote, ray.GetPoint(rayDistance) + Vector3.up * height, Quaternion.identity);
+               // markerObject.position = ray.GetPoint(rayDistance);
+            }
+        }
+
+            }
+}
